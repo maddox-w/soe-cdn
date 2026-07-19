34f5b7905aff10a3df3c14516e25369a533d5de7
Maddox W <cormadclaude@proton.me>
2026-07-19 10:09:19 -0400

    Brine Master launch: /brine-maker page (v2bm), /camion hero updates (logo/lede/3 CTAs), hi-res Camion logos, BM assets+PDFs, site-wide Request-Info fixer

--- boot-head.js
+++ boot-head.js
@@ -12,7 +12,7 @@
     else if(path==='/mulch-mule')p='mulch-mule';
     else if(path==='/remote-controlled-mowers')p='energreen';
     else if(path==='/hydrospade'||path==='/hydrospade-trucks'||path==='/hydrospade-trailers')p='hydrospade';
-    else if(path==='/camion')p='camion';
+    else if(path==='/camion'||path==='/brine-maker')p='camion';
     else if(path==='/new-customer-setup')p='newcustomer';
     document.documentElement.setAttribute('data-soe-page',p);
   })();
